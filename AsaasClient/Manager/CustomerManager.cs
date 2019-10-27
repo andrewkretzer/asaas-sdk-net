e37940191fb07481278c465f86e738f1bb79ec39
Andrew kretzer <andrew.kretzer94@outlook.com>
2019-10-26 23:28:54 -0300

    adding new operations with customer

--- AsaasClient/Manager/CustomerManager.cs
+++ AsaasClient/Manager/CustomerManager.cs
@@ -12,10 +12,37 @@ namespace AsaasClient.Manager
 
         public CustomerManager(ApiSettings settings) : base(settings) { }
 
-        public async Task<ResponseObject<CreatedCustomer>> Create(Object obj)
+        public async Task<ResponseObject<CreatedCustomer>> Create(CreateCustomerRequest requestObj)
         {
-            var httpResponseMessage = await PostAsync(CUSTOMERS_URL, obj);
+            var httpResponseMessage = await PostAsync(CUSTOMERS_URL, requestObj);
+
             var responseObject = new ResponseObject<CreatedCustomer>(httpResponseMessage);
+            return responseObject;
+        }
+
+        public async Task<ResponseObject<RetrievedCustomer>> GetById(string customerId)
+        {
+            if (string.IsNullOrWhiteSpace(customerId)) throw new ArgumentException("customerId cannot be null");
+
+            var queryMap = new Map();
+            queryMap.Add("id", customerId);
+
+            using var httpResponseMessage = await GetAsync(CUSTOMERS_URL, queryMap);
+
+            var responseObject = new ResponseObject<RetrievedCustomer>(httpResponseMessage);
+            return responseObject;
+        }
+
+        public async Task<ResponseObject<DeletedCustomer>> Delete(string customerId)
+        {
+            if (string.IsNullOrWhiteSpace(customerId)) throw new ArgumentException("customerId cannot be null");
+
+            var queryMap = new Map();
+            queryMap.Add("id", customerId);
+
+            using var httpResponseMessage = await DeleteAsync(CUSTOMERS_URL, queryMap);
+
+            var responseObject = new ResponseObject<DeletedCustomer>(httpResponseMessage);
             return responseObject;
         }
     }
