aca1a5c65d85e8bf69764dba020399b0c73eecd2
Andrew kretzer <andrew.kretzer94@outlook.com>
2019-10-27 20:07:57 -0300

    Adding class responsable for the query filters in customer list request

---
+++ AsaasClient/Models/Customer/CustomerListFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AsaasClient.Models.Customer
+{
+    public class CustomerListFilter
+    {
+        public List<string> Names { get; set; }
+        public List<string> Emails { get; set; }
+        public List<string> CpfCnpjs { get; set; }
+        public List<string> ExternalReferences { get; set; }
+    }
+}
