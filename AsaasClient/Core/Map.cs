605facc63700c1c5d8f25de8c73bd5dfe59cc7ea
Andrew kretzer <andrew.kretzer94@outlook.com>
2019-10-26 23:26:28 -0300

    creating map class to facilitate the use of keyvaluepair list

---
+++ AsaasClient/Core/Map.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AsaasClient.Core
+{
+    public class Map
+    {
+        private readonly List<KeyValuePair<string, object>> keyValuePairList = new List<KeyValuePair<string, object>>();
+
+        public int Count
+        {
+            get
+            {
+                return keyValuePairList.Count;
+            }
+        }
+
+        public void Add(string key, object value)
+        {
+            var keyValuePair = new KeyValuePair<string, object>(key, value);
+            keyValuePairList.Add(keyValuePair);
+        }
+
+        public KeyValuePair<string, object> Get(int index)
+        {
+            return keyValuePairList[index];
+        }
+    }
+}
