280545babdba78739b4107dcb1a0aece82e4cff1
Andrew kretzer <andrew.kretzer94@outlook.com>
2019-10-26 23:24:23 -0300

    Creating PersonType enum

---
+++ AsaasClient/Models/Enums/PersonType.cs
@@ -0,0 +1,20 @@
+﻿namespace AsaasClient.Models.Enums
+{
+    public enum PersonType
+    {
+        FISICA, JURIDICA
+    }
+
+    public static class PersonTypeExtension
+    {
+        public static bool IsLegalPerson(this PersonType personType)
+        {
+            return personType == PersonType.JURIDICA;
+        }
+
+        public static bool IsIndividualPerson(this PersonType personType)
+        {
+            return personType == PersonType.FISICA;
+        }
+    }
+}
