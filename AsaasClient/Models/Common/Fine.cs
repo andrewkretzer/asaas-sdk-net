90c3aa0372eb62c49e2fbf081aef81c970b9b902
Andrew kretzer <andrew.kretzer94@outlook.com>
2019-10-27 21:53:48 -0300

    Creating common models for payment

---
+++ AsaasClient/Models/Common/Fine.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace AsaasClient.Models.Common
+{
+    public class Fine
+    {
+        [JsonProperty(PropertyName = "value")]
+        public decimal Value { get; set; }
+    }
+}
---
+++ AsaasClient/Models/Common/Interest.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace AsaasClient.Models.Common
+{
+    public class Interest
+    {
+        [JsonProperty(PropertyName = "value")]
+        public decimal Value { get; set; }
+    }
+}
