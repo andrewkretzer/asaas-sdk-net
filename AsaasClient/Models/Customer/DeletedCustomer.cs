61bcc1d9e54599ccf867707da7e3016245fe1e4d
douglasgiovanella <giovanelladouglas@gmail.com>
2019-11-01 23:10:03 -0300

    Criado BaseDeleted para ser a classe pai de todas as classes de resposta de remoção da API

--- AsaasClient/Models/Customer/DeletedCustomer.cs
+++ AsaasClient/Models/Customer/DeletedCustomer.cs
@@ -1,13 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using AsaasClient.Models.Common.Base;
 
 namespace AsaasClient.Models.Customer
 {
-    public class DeletedCustomer
-    {
-        [JsonProperty(PropertyName = "id")]
-        public string Id { get; set; }
-
-        [JsonProperty(PropertyName = "deleted")]
-        public bool Deleted { get; set; }
-    }
+    public class DeletedCustomer : BaseDeleted { }
 }
