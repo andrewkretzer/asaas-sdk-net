c3de1badb1bec6ea41f8307a71873d1050d8f5c0
Andrew kretzer <andrew.kretzer94@outlook.com>
2019-11-16 11:40:56 -0300

    Adicionando models para criação de cobranças

---
+++ AsaasClient/Models/Common/CreditCard.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace AsaasClient.Models.Common
+{
+    public class CreditCard
+    {
+        [JsonProperty(PropertyName = "creditCardNumber")]
+        public string CreditCardNumber { get; set; }
+
+        [JsonProperty(PropertyName = "creditCardBrand")]
+        public string CreditCardBrand { get; set; }
+
+        [JsonProperty(PropertyName = "creditCardToken")]
+        public string CreditCardToken { get; set; }
+    }
+}
---
+++ AsaasClient/Models/Payment/Split.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace AsaasClient.Models.Payment
+{
+    public class Split
+    {
+        [JsonProperty(PropertyName = "walletId")]
+        public string WalletId { get; set; }
+
+        [JsonProperty(PropertyName = "fixedValue")]
+        public string FixedValue { get; set; }
+
+        [JsonProperty(PropertyName = "percentualValue")]
+        public string PercentualValue { get; set; }
+    }
+}
