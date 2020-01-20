3bfeb8a311806f870d5a6497123e6ca6096864d7
Andrew Kretzer <kretzer.andrew@outlook.com>
2020-01-20 20:41:23 -0300

    Adicionando feature de parcelamento

---
+++ AsaasClient.V3/Managers/InstallmentManager.cs
@@ -0,0 +1,43 @@
+﻿using AsaasClient.Core;
+using AsaasClient.Core.Response;
+using AsaasClient.V3.Models.Installment;
+using System;
+using System.Threading.Tasks;
+
+namespace AsaasClient.V3.Managers
+{
+    public class InstallmentManager : BaseManager
+    {
+        private const string INSTALLMENTS_URL = "/installments";
+
+        public InstallmentManager(ApiSettings settings) : base(settings, 3) { }
+
+        public async Task<ResponseObject<Installment>> Find(string id)
+        {
+            return await GetAsync<Installment>(INSTALLMENTS_URL, id);
+        }
+
+        public async Task<ResponseList<Installment>> List(int offset, int limit)
+        {
+            return await GetListAsync<Installment>(INSTALLMENTS_URL, offset, limit);
+        }
+        public async Task<ResponseObject<DeletedInstallment>> Delete(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentException("Id is required");
+
+            var responseObject = await DeleteAsync<DeletedInstallment>(INSTALLMENTS_URL, id);
+
+            return responseObject;
+        }
+
+        public async Task<ResponseObject<Installment>> Refund(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentException("Id is required");
+
+            var url = $"{INSTALLMENTS_URL}/{id}/refund";
+            var responseObject = await PostAsync<Installment>(url, new object());
+
+            return responseObject;
+        }
+    }
+}
---
+++ AsaasClient.V3/Models/Installment/Installment.cs
@@ -0,0 +1,39 @@
+﻿using AsaasClient.V3.Models.Common.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+namespace AsaasClient.V3.Models.Installment
+{
+    public class Installment
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "value")]
+        public decimal Value { get; set; }
+
+        [JsonProperty(PropertyName = "paymentValue")]
+        public decimal PaymentValue { get; set; }
+
+        [JsonProperty(PropertyName = "installmentCount")]
+        public int InstallmentCount { get; set; }
+
+        [JsonProperty(PropertyName = "billingType")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public BillingType BillingType { get; set; }
+
+        [JsonProperty(PropertyName = "paymentDate")]
+        public DateTime? PaymentDate { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "expirationDay")]
+        public DateTime ExpirationDay { get; set; }
+
+        [JsonProperty(PropertyName = "customer")]
+        public string CustomerId { get; set; }
+
+    }
+}
