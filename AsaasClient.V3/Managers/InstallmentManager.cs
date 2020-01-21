14e1d0aa5bc850ffc6584a27f20e6d5208207ed7
Andrew Kretzer <kretzer.andrew@outlook.com>
2020-01-20 21:35:14 -0300

    Padronizando parâmetro

--- AsaasClient.V3/Managers/InstallmentManager.cs
+++ AsaasClient.V3/Managers/InstallmentManager.cs
@@ -12,29 +12,29 @@ namespace AsaasClient.V3.Managers
 
         public InstallmentManager(ApiSettings settings) : base(settings, 3) { }
 
-        public async Task<ResponseObject<Installment>> Find(string id)
+        public async Task<ResponseObject<Installment>> Find(string installmentId)
         {
-            return await GetAsync<Installment>(INSTALLMENTS_URL, id);
+            return await GetAsync<Installment>(INSTALLMENTS_URL, installmentId);
         }
 
         public async Task<ResponseList<Installment>> List(int offset, int limit)
         {
             return await GetListAsync<Installment>(INSTALLMENTS_URL, offset, limit);
         }
-        public async Task<ResponseObject<DeletedInstallment>> Delete(string id)
+        public async Task<ResponseObject<DeletedInstallment>> Delete(string installmentId)
         {
-            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentException("Id is required");
+            if (string.IsNullOrWhiteSpace(installmentId)) throw new ArgumentException("installmentId is required");
 
-            var responseObject = await DeleteAsync<DeletedInstallment>(INSTALLMENTS_URL, id);
+            var responseObject = await DeleteAsync<DeletedInstallment>(INSTALLMENTS_URL, installmentId);
 
             return responseObject;
         }
 
-        public async Task<ResponseObject<Installment>> Refund(string id)
+        public async Task<ResponseObject<Installment>> Refund(string installmentId)
         {
-            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentException("Id is required");
+            if (string.IsNullOrWhiteSpace(installmentId)) throw new ArgumentException("installmentId is required");
 
-            var url = $"{INSTALLMENTS_URL}/{id}/refund";
+            var url = $"{INSTALLMENTS_URL}/{installmentId}/refund";
             var responseObject = await PostAsync<Installment>(url, new object());
 
             return responseObject;
