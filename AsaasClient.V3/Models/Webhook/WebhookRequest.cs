46d96b9ac075fa658bcecf33c7e32088ce59f36c
Andrew Kretzer <kretzer.andrew@outlook.com>
2020-01-25 19:57:33 -0300

    Adicionando models para webhooks

---
+++ AsaasClient.V3/Models/Webhook/WebhookRequest.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace AsaasClient.V3.Models.Webhook
+{
+    public class WebhookRequest
+    {
+        [JsonProperty(PropertyName = "url")]
+        public string Url { get; set; }
+
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "apiVersion")]
+        public int ApiVersion { get; set; }
+
+        [JsonProperty(PropertyName = "enabled")]
+        public bool Enabled { get; set; }
+
+        [JsonProperty(PropertyName = "interrupted")]
+        public bool Interrupted { get; set; }
+
+        [JsonProperty(PropertyName = "authToken")]
+        public string AuthToken { get; set; }
+    }
+}
