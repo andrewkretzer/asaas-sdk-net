f8581e1ff8840eb94a478084aa69631aa4e8b2f0
Andrew Kretzer <kretzer.andrew@outlook.com>
2020-01-25 15:54:42 -0300

    Adicionando models para request e response de nota fiscal para assinatura

---
+++ AsaasClient.V3/Models/Subscription/CreateInvoiceSettingsRequest.cs
@@ -0,0 +1,38 @@
+﻿using AsaasClient.V3.Models.Common;
+using AsaasClient.V3.Models.Subscription.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace AsaasClient.V3.Models.Subscription
+{
+    public class CreateInvoiceSettingsRequest
+    {
+        [JsonProperty(PropertyName = "municipalServiceId")]
+        public string MunicipalServiceId { get; set; }
+
+        [JsonProperty(PropertyName = "municipalServiceCode")]
+        public string MunicipalServiceCode { get; set; }
+
+        [JsonProperty(PropertyName = "municipalServiceName")]
+        public string MunicipalServiceName { get; set; }
+
+        [JsonProperty(PropertyName = "deductions")]
+        public decimal Deductions { get; set; }
+
+        [JsonProperty(PropertyName = "effectiveDatePeriod")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public EffectiveDatePeriod EffectiveDatePeriod { get; set; }
+
+        [JsonProperty(PropertyName = "daysBeforeDueDate")]
+        public int DaysBeforeDueDate { get; set; }
+
+        [JsonProperty(PropertyName = "receivedOnly")]
+        public bool ReceivedOnly { get; set; }
+
+        [JsonProperty(PropertyName = "observations")]
+        public string Observations { get; set; }
+
+        [JsonProperty(PropertyName = "taxes")]
+        public Taxes Taxes { get; set; }
+    }
+}
