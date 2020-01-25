3c62af13622f931f15c5d9c52498599c2cdeb85c
Andrew Kretzer <kretzer.andrew@outlook.com>
2020-01-25 18:15:29 -0300

    Adicionando endpoint de Wallet e transferência

---
+++ AsaasClient.V3/Managers/WalletManager.cs
@@ -0,0 +1,21 @@
+﻿using AsaasClient.Core;
+using AsaasClient.Core.Response;
+using AsaasClient.V3.Models.Wallet;
+using System.Threading.Tasks;
+
+namespace AsaasClient.V3.Managers
+{
+    public class WalletManager : BaseManager
+    {
+        private const string WALLET_URL = "/wallets";
+
+        public WalletManager(ApiSettings settings) : base(settings, 3) { }
+
+        public async Task<ResponseList<Wallet>> List()
+        {
+            var responseList = await GetListAsync<Wallet>(WALLET_URL, 0, 0);
+
+            return responseList;
+        }
+    }
+}
