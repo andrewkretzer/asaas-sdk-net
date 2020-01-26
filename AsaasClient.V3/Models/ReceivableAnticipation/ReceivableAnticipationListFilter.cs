df539a8d9194145fb49c383f8f7b16846dee2f95
Andrew Kretzer <kretzer.andrew@outlook.com>
2020-01-25 21:24:31 -0300

    Adicionando models para antecipação

---
+++ AsaasClient.V3/Models/ReceivableAnticipation/ReceivableAnticipationListFilter.cs
@@ -0,0 +1,14 @@
+﻿using AsaasClient.Core;
+using AsaasClient.V3.Models.ReceivableAnticipation.Enums;
+
+namespace AsaasClient.V3.Models.ReceivableAnticipation
+{
+    public class ReceivableAnticipationListFilter : RequestParameters
+    {
+        public ReceivableAnticipationStatus? ReceivableAnticipationStatus
+        {
+            get => Get<ReceivableAnticipationStatus?>("status");
+            set => Add("status", value);
+        }
+    }
+}
