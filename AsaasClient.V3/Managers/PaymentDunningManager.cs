285edf6275294d0d1e1011e659bdc4cf4f50c56a
Andrew Kretzer <kretzer.andrew@outlook.com>
2020-08-08 17:42:46 -0300

    Adicionando manager para recuperação

---
+++ AsaasClient.V3/Managers/PaymentDunningManager.cs
@@ -0,0 +1,104 @@
+﻿using AsaasClient.Core;
+using AsaasClient.Core.Response;
+using AsaasClient.V3.Models.PaymentDunning;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AsaasClient.V3.Managers
+{
+    public class PaymentDunningManager : BaseManager
+    {
+        private const string PAYMENT_DUNNING_URL = "/paymentDunnings";
+
+        public PaymentDunningManager(ApiSettings settings) : base(settings, 3) { }
+
+        public async Task<ResponseObject<PaymentDunning>> Create(CreatePaymentDunningRequest requestObj)
+        {
+            var responseObject = await PostMultipartFormDataContentAsync<PaymentDunning>(PAYMENT_DUNNING_URL, requestObj);
+
+            return responseObject;
+        }
+
+        public async Task<ResponseObject<SimulatedPaymentDunning>> Simulate(SimulatePaymentDunningRequest requestObj)
+        {
+            var url = $"{PAYMENT_DUNNING_URL}/simulate";
+            var responseObject = await PostAsync<SimulatedPaymentDunning>(url, requestObj);
+
+            return responseObject;
+        }
+
+        public async Task<ResponseObject<PaymentDunning>> Find(string paymentDunningId)
+        {
+            if (string.IsNullOrWhiteSpace(paymentDunningId)) throw new ArgumentException("paymentDunningId is required");
+
+            var url = $"{PAYMENT_DUNNING_URL}/{paymentDunningId}";
+            var responseObject = await GetAsync<PaymentDunning>(url);
+
+            return responseObject;
+        }
+
+        public async Task<ResponseList<PaymentDunning>> List(int offset, int limit, PaymentDunningListFilter filter = null)
+        {
+            var queryMap = new RequestParameters();
+
+            if (filter != null)
+            {
+                queryMap.AddRange(filter);
+            }
+
+            var responseList = await GetListAsync<PaymentDunning>(PAYMENT_DUNNING_URL, offset, limit, queryMap);
+
+            return responseList;
+        }
+
+        public async Task<ResponseList<PaymentDunningEventHistory>> ListEventHistory(string paymentDunningId, int offset, int limit)
+        {
+            if (string.IsNullOrWhiteSpace(paymentDunningId)) throw new ArgumentException("paymentDunningId is required");
+
+            var url = $"{PAYMENT_DUNNING_URL}/{paymentDunningId}/history";
+            var responseList = await GetListAsync<PaymentDunningEventHistory>(url, offset, limit, new RequestParameters());
+
+            return responseList;
+        }
+
+        public async Task<ResponseList<PaymentDunningPartialPayments>> ListPartialPaymentsReceived(string paymentDunningId, int offset, int limit)
+        {
+            if (string.IsNullOrWhiteSpace(paymentDunningId)) throw new ArgumentException("paymentDunningId is required");
+
+            var url = $"{PAYMENT_DUNNING_URL}/{paymentDunningId}/partialPayments";
+            var responseList = await GetListAsync<PaymentDunningPartialPayments>(url, offset, limit, new RequestParameters());
+
+            return responseList;
+        }
+
+        public async Task<ResponseList<PaymentDunningPaymentAvailable>> ListPaymentsAvailableForDunning(int offset, int limit)
+        {
+            var url = $"{PAYMENT_DUNNING_URL}/paymentsAvailableForDunning";
+            var responseList = await GetListAsync<PaymentDunningPaymentAvailable>(url, offset, limit, new RequestParameters());
+
+            return responseList;
+        }
+
+        public async Task<ResponseObject<PaymentDunning>> ResendDocument(string paymentDunningId, List<FileInfo> requestObj)
+        {
+            if (string.IsNullOrWhiteSpace(paymentDunningId)) throw new ArgumentException("paymentDunningId is required");
+
+            var url = $"{PAYMENT_DUNNING_URL}/{paymentDunningId}/documents";
+            var responseObject = await PostAsync<PaymentDunning>(url, requestObj);
+
+            return responseObject;
+        }
+
+        public async Task<ResponseObject<PaymentDunning>> Cancel(string paymentDunningId)
+        {
+            if (string.IsNullOrWhiteSpace(paymentDunningId)) throw new ArgumentException("paymentDunningId is required");
+
+            var url = $"{PAYMENT_DUNNING_URL}/{paymentDunningId}/cancel";
+            var responseObject = await PostAsync<PaymentDunning>(url, new RequestParameters());
+
+            return responseObject;
+        }
+    }
+}
