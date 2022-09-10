e45a82fdcc5ff07e970b1d49eceba5d695daab7f
Gian <gian21@gmail.com>
2022-09-10 10:14:26 -0300

    Corrige a resposta de erro nula
    Corrige o erro vindo nulo após JsonConvert.DeserializeObject<List<Error>

--- AsaasClient/Core/Response/Error.cs
+++ AsaasClient/Core/Response/Error.cs
@@ -1,10 +1,12 @@
-﻿
+﻿using Newtonsoft.Json;
 namespace AsaasClient.Core.Response
 {
     public class Error
     {
+        [JsonProperty(PropertyName = "code")]
         public string Code { get; internal set; }
 
+        [JsonProperty(PropertyName = "description")]
         public string Description { get; internal set; }
     }
 }
